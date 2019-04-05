d0fe79812539c255405148bba533fa42169788d0
Eddy703 <eddylei070300@gmail.com>
2019-04-05 23:33:50 +0800

    database write in added

--- CNNScrape/spiders/CNN.py
+++ CNNScrape/spiders/CNN.py
@@ -6,9 +6,10 @@ import re
 import sqlite3
 """
  TO-DO:
- - DB issue
- - Index error (optional)
+ - Missing info in data DataBase
+ - Index error
  Done:
+ - DataBase write in
  - Get Catagories from homepage
  - Store titles and corresponding links
  - Grab article author, time and content
@@ -26,7 +27,7 @@ def create_table():
 def transaction_bld(sql):
     global connection; global c; global transaction
     transaction.append(sql)
-    if len(transaction) > 5:
+    if len(transaction) > 4:
         c.execute("BEGIN TRANSACTION")
         for s in transaction:
             try:
@@ -42,7 +43,6 @@ create_table()
 class CnnXpathSpider(scrapy.Spider):
     name = 'CNN_xpath'
     start_urls = ['https://edition.cnn.com/']
-    create_table()
 
     # Initial parse
     def parse(self, response):
@@ -76,6 +76,8 @@ class CnnXpathSpider(scrapy.Spider):
                     continue
                 elif re.search(r"(\/travel)", art):
                     yield scrapy.Request(art, callback = self.tra_content)
+                elif re.search(r'\/travel\/special', art):
+                    continue
                 else:
                     yield scrapy.Request(art, callback = self.norm_content)
 
@@ -85,7 +87,7 @@ class CnnXpathSpider(scrapy.Spider):
         author = st[0].replace(", CNN", "")
         time = st[-1]
         content = "\n".join(response.css("div.Article__body *::text").getall()).strip()
-        sql = """INSERT INTO Article(catagory = ?, time = ?, author = ?, content = ?)""".format(catagory, time, author, content)
+        sql = """INSERT INTO Article (catagory, time, author, content) VALUES ("{}", "{}", "{}", "{}");""".format(catagory, time, author, content)
         transaction_bld(sql)
 
     def norm_content(self, response):
@@ -94,9 +96,9 @@ class CnnXpathSpider(scrapy.Spider):
         editorial = response.css("span.metadata__byline__author a::text").get()
         time = response.css("p.update-time::text").get()
         if response.css("span.metadata__byline__author::text"):
-            author = author = response.css("div.Article_subtitle::text").get()
-            sql = """INSERT INTO Article(catagory = ?, time = ?, author = ?, editorial = ?, content = ?)""".format(catagory, time, author, editorial, content)
+            author = response.css("div.Article_subtitle::text").get()
+            sql = """INSERT INTO Article (catagory, time, author, editorial, content) VALUES ("{}", "{}", "{}", "{}", "{}");""".format(catagory, time, author, editorial, content)
             transaction_bld(sql)
         else:
-            sql = """INSERT INTO Article(catagory = ?, time = ?, editorial = ?, content = ?)""".format(catagory, time, editorial, content)
+            sql = """INSERT INTO Article (catagory, time, editorial , content) VALUES ("{}", "{}", "{}", "{}");""".format(catagory, time, editorial, content)
             transaction_bld(sql)
